dbaf57cb12e3f583325ff044fb19edb8b7900aa6
Dasstienn <dasstienn@gmail.com>
2023-04-04 20:07:12 -0700

    removed hello from footer and added redirect

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import classes from "./Footer.module.css"
-
+import classes from "./Footer.module.css";
 
 export default class Footer extends React.Component {
-
-
-    render() {
-        return (
-            <div className={classes.footer}>
-                <p>Hello</p>
-            </div>
-        )
-    }
+  render() {
+    return <div className={classes.footer}></div>;
+  }
 }
